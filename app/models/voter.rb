f2d1c16cd70a58cf71f3e7d9c15db20f1fcbe8b2
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2016-05-29 18:45:37 +0200

    Correct bug on name function (2nd edition)

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -2,13 +2,6 @@ class Voter < ActiveRecord::Base
   has_secure_token
   after_create :send_email
 
-  def name
-    first_name if last_name.nil? and not first_name.nil?
-    last_name if first_name.nil? and not last_name.nil?
-    'No Name' if first_name.nil? and last_name.nil?
-    "#{first_name} #{last_name.upcase}"
-  end
-
   private
   def send_email
     VoterMailer.vote_email(self).deliver
