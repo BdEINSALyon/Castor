4d44dde2e33831c05cf3a383dfed2f0cf864f4f5
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2016-05-29 18:43:22 +0200

    Correct bug on name function

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -3,6 +3,9 @@ class Voter < ActiveRecord::Base
   after_create :send_email
 
   def name
+    first_name if last_name.nil? and not first_name.nil?
+    last_name if first_name.nil? and not last_name.nil?
+    'No Name' if first_name.nil? and last_name.nil?
     "#{first_name} #{last_name.upcase}"
   end
 
