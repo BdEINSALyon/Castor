ebf3885539e41abdf89b4c8d86fababa36b9745b
Philippe Vienne <philippe.vienne@insa-lyon.fr>
2016-05-29 15:43:39 +0200

    Apply migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529130845) do
+ActiveRecord::Schema.define(version: 20160529133752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,5 +33,28 @@ ActiveRecord::Schema.define(version: 20160529130845) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+
+  create_table "listings", force: :cascade do |t|
+    t.string   "name"
+    t.text     "participants"
+    t.string   "website"
+    t.string   "facebook"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "voters", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.boolean  "voted"
+    t.string   "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "listing_id"
+  end
 
 end
