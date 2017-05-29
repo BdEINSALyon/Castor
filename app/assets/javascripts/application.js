6a648c01b9fba08bd79463d0d38cbb48798b1300
Philippe Vienne <philippe@vienne.me>
2017-05-29 10:15:53 +0200

    Apply Raynal edits

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require jquery.csv.js
 //= require_tree .
 
-$(function(){
-    $('#launch').click(function(){
+$(function () {
+    $('#launch').click(function () {
         console.log('load!');
         $('#select-form').hide();
         $('#progress').toggleClass('hide');
@@ -24,7 +25,7 @@ $(function(){
         return false;
     });
 
-    if(document.getElementById('new_vote')) {
+    if (document.getElementById('new_vote')) {
 
         var last_selected = null;
 
@@ -39,14 +40,14 @@ $(function(){
 
         var can_submit = false;
 
-        $('#confirmButton').click(function(){
+        $('#confirmButton').click(function () {
             can_submit = true;
             $('form').submit();
             $('#confirmDialog').modal('hide');
         });
 
-        $('form').submit(function(){
-            if(!can_submit){
+        $('form').submit(function () {
+            if (!can_submit) {
                 $('#confirmDialog').modal('show');
             } else {
                 can_submit = true;
@@ -60,45 +61,43 @@ $(function(){
  * Parse CSV and put data in table.
  */
 function parseCSV(filePath) {
-    console.log(filePath);
-    // Read file and then (callback) fill the table
-    readFile(filePath, function (e) {
-        const data = $.csv.toObjects(e.target.result);
-        var i = 0;
-        var start = new Date();
-        var loadNext = function(){
-            var $progress = $('.progress-bar');
-            if(i>=data.length){
-                $progress.css('width','100%').html('It\'s the end');
-                setTimeout(function(){
-                    $('#select-form').show();
-                    $('#progress').toggleClass('hide');
-                },5000);
-                return;
-            }
-            var v = data[i];
-            i++;
-            var position = Math.round((i/data.length)*100);
-            $progress.css('width',position+'%').html(position+'%');
-            if(v.email)
-                $.post('',{voter:{first_name:v.first_name, last_name: v.last_name, email: v.email}},function(result){
-                    console.log(result);
-                    loadNext();
-                });
-            else
-                loadNext();
-        };
-        loadNext();
-    });
-}
 
-/**
- * read text input
- */
-function readFile(filePath, onLoadCallback) {
-    var reader = new FileReader();
-    reader.onload = onLoadCallback;
-    if (filePath.files && filePath.files[0]) {
-        reader.readAsText(filePath.files[0]);
-    }
+    console.log(filePath);
+    if (filePath.files && filePath.files[0])
+        Papa.parse(filePath.files[0], {
+            header: true,
+            complete: function (results, parser) {
+                const voters = results.data;
+                if(voters) {
+                    var i = 0;
+                    var start = new Date();
+                    var $progress = $('.progress-bar');
+                    var $text = $('.progress-text');
+                    var loadNext = function(){
+                        if(i>=voters.length){
+                            $text.html('Go drink a beer!');
+                            $progress.css('width','100%').html('It\'s the end');
+                            setTimeout(function(){
+                                $('#select-form').show();
+                                $('#progress').toggleClass('hide');
+                            },5000);
+                            return;
+                        }
+                        var v = voters[i];
+                        i++;
+                        $text.html('Sending for '+v.first_name+' '+v.last_name+' ('+v.email+') ...');
+                        var position = Math.round((i/voters.length)*100);
+                        $progress.css('width',position+'%').html(position+'%');
+                        if(v.email)
+                            $.post('',{voter:{first_name:v.first_name, last_name: v.last_name, email: v.email}},function(result){
+                                console.log(result.voter.email);
+                                loadNext();
+                            });
+                        else
+                            loadNext();
+                    };
+                    loadNext();
+                }
+            }
+        });
 }
